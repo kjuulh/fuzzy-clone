9d1babe1ccbf7f2a2046e4cb971edf088e2f4e2b
Kasper Juul Hermansen <contact@kjuulh.io>
2025-11-12 11:21:59 +0100

    feat: replace cobra with urfav for better env support (#11)

--- internal/shell/init.go
+++ internal/shell/init.go
@@ -1,44 +1,45 @@
 package shell
 
 import (
+	"context"
 	"fmt"
 	"fuzzy-clone/shell/fish"
 	"fuzzy-clone/shell/zsh"
 
-	"github.com/spf13/cobra"
+	"github.com/urfave/cli/v3"
 )
 
-func InitCmd() *cobra.Command {
-	cmd := cobra.Command{
-		Use: "init",
-	}
-
-	cmd.AddCommand(
-		zshCmd(),
-		fishCmd(),
-	)
-
-	return &cmd
-}
-
-func zshCmd() *cobra.Command {
-	return &cobra.Command{
-		Use: "zsh",
-		Run: func(cmd *cobra.Command, args []string) {
-			zshScript := zsh.Get()
-
-			fmt.Println(zshScript)
+func InitCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "init",
+		Usage: "Initialize shell integration",
+		Commands: []*cli.Command{
+			zshCmd(),
+			fishCmd(),
 		},
 	}
 }
 
-func fishCmd() *cobra.Command {
-	return &cobra.Command{
-		Use: "fish",
-		Run: func(cmd *cobra.Command, args []string) {
-			fishScript := fish.Get()
+func zshCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "zsh",
+		Usage: "Output zsh initialization script",
+		Action: func(ctx context.Context, c *cli.Command) error {
+			zshScript := zsh.Get()
+			fmt.Println(zshScript)
+			return nil
+		},
+	}
+}
 
+func fishCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "fish",
+		Usage: "Output fish initialization script",
+		Action: func(ctx context.Context, c *cli.Command) error {
+			fishScript := fish.MustGet()
 			fmt.Println(fishScript)
+			return nil
 		},
 	}
 }
